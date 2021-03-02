2dfd48c5b6bf73a99104bae414abd09789a13000
Luis Melendez <luis.melendez@busolinea.com>
2021-03-02 00:57:57 -0600

    added middleware for printing routes logs

--- salesApi/src/routes/main.js
+++ salesApi/src/routes/main.js
@@ -2,7 +2,21 @@ import halson from 'halson'
 
 module.exports = app => {
     const Router = app.routing
-    const HAL = app.libs.hal    
+    const HAL = app.libs.hal  
+    
+      app.use(function (req, res, next) {
+            console.log(`Time:  ${Date.now()} Request: ${req.originalUrl}`);
+            next();
+      });
+
+      app.get('/alive', async (req, res) => {       
+            res.json(HAL.MainHAL())           
+      });
+
+      app.get('/', async (req, res) => {
+
+            res.json(HAL.MainHAL()) 
+      });
     Router.route('/alive').get(async (req, res) => {       
           res.json(HAL.MainHAL())           
     });
--- usersApi/src/routes/main.js
+++ usersApi/src/routes/main.js
@@ -1,14 +1,26 @@
-import halson from 'halson'
-
 module.exports = app => {
-    const Router = app.routing
-    const HAL = app.libs.hal    
-    Router.route('/alive').get(async (req, res) => {       
-          res.json(HAL.MainHAL())           
-    });
+      const Router = app.routing
+      const HAL = app.libs.hal   
+    
+      app.use(function (req, res, next) {
+            console.log(`Time:  ${Date.now()} Request: ${req.originalUrl}`);
+            next();
+      });
 
-    Router.route('/').get(async (req, res) => {
-        
-          res.json(HAL.MainHAL()) 
-    });
+      app.get('/alive', async (req, res) => {       
+            res.json(HAL.MainHAL())           
+      });
+
+      app.get('/', async (req, res) => {
+
+            res.json(HAL.MainHAL()) 
+      });
+      Router.route('/alive').get(async (req, res) => {       
+            res.json(HAL.MainHAL())           
+      });
+
+      Router.route('/').get(async (req, res) => {
+            
+            res.json(HAL.MainHAL()) 
+      });
 };
